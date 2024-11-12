3212bed32c37d6eaa2d583831929d89e66076a76
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2024-11-12 18:59:35 -0300

    first commit

---
+++ aula01/index.js
@@ -0,0 +1,13 @@
+// const {soma} = require("./calculadora.js")
+
+// let resultadoSoma = soma(10, 5)
+
+// console.log("O resultado da soma é " + resultadoSoma)
+
+const {verificarVelocidade} = require("./verificadorVelocidade.js")
+const prompt = require("prompt-sync")()
+
+let velocidadeAtual = prompt("Digite a velocidade atual: ")
+let velocidadeMaxima = prompt("Digite a velocidade máxima: ")
+
+verificarVelocidade(velocidadeAtual, velocidadeMaxima)
